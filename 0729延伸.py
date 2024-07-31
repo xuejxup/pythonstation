6fed8e94f1b071a2ed666c4f4f36fb599baba3db
xuejxup <xuejxup@gmail.com>
2024-07-31 10:14:51 +0800

    斐波那契数列

---
+++ 0729延伸.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 09:31:12 2024
+
+@author: xuejxup
+"""
+
+a=1
+b=1
+print(a)
+print(b)
+
+
+for i in range(10):
+    c=a+b
+    print(c) 
+    a=b 
+    b=c 
+    
