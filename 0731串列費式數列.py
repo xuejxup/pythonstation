24f4e5ee54fcb0cf212d4b1cb5e524d542247b29
xuejxup <xuejxup@gmail.com>
2024-08-07 10:47:17 +0800

    Add files via upload

---
+++ 0731串列費式數列.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 22:26:29 2024
+
+@author: xuejxup
+"""
+
+
+q=int(input("輸入次數:"))
+a=[1,1,2,3,5,8,13,21,34,55,89,144]
+
+
+
+for i in range(q+2):
+    
+    print(a[i],end=",") 
+
